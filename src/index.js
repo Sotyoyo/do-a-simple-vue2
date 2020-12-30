6d5243abceba43813f422328f6b7db46b6d7665d
sheldon <sotyoyotao@gmail.com>
2020-12-31 07:53:46 +0800

    step four: get vdom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { initMixin } from "./init.js";
+import { renderMixin } from "./render.js";
 
 function Vue(options) {
   this._init(options); // 初始化操作， 后面还有组件， 组件也需要用到这个方法
 }
 
 initMixin(Vue);
+renderMixin(Vue);
 
 export default Vue;
